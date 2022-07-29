d37e96efb0cfb40324387b99292938f22502b356
Final233 <23329853+Final233@users.noreply.github.com>
2022-07-29 09:37:19 +0800

    2022-07-29-09:37:19

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [ -z $@ ]; then
+    version='3.8.13'
+else
+    version="$@"
+fi
+
+APPDIR="python-$version"
+PKGNAME="Python-$version"
+MAKE_OPT="./configure --prefix=${APPDIR} \
+–-enable-optimizations"
+
+_python_make_install(){
+    wget -c https://www.python.org/ftp/python/$version/$PKGNAME.tar.xz
+    cd python-$version && $MAKE_OPT && make -j $(nproc) && make install 
+}
+
+_python_make_install "$@"
+
+gh release delete ${APPDIR} -y
+
+# gh release create ${PKGNAME} ./*.tar.xz --title "${PKGNAME} (beta)" --notes "this is a nginx beta release" --prerelease
+gh release create ${APPDIR} ./*.tar.xz --title "${APPDIR}" --notes "this is a python release" --prerelease
